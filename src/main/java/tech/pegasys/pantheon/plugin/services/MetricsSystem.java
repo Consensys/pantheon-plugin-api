52d67f4737f1df02b35ebdc071afbcb0fd773635
Mark Terry <mark.terry@consensys.net>
2019-09-02 11:49:35 +1000

    [PIE-1859] Additional PR fixes.

--- src/main/java/tech/pegasys/pantheon/plugin/services/MetricsSystem.java
+++ src/main/java/tech/pegasys/pantheon/plugin/services/MetricsSystem.java
@@ -21,7 +21,7 @@ import java.util.function.DoubleSupplier;
 import java.util.function.IntSupplier;
 import java.util.function.LongSupplier;
 
-/** An interface for creating various Metrics components and retrieving observations. */
+/** An interface for creating various Metrics components. */
 public interface MetricsSystem {
 
   /**
@@ -29,7 +29,7 @@ public interface MetricsSystem {
    *
    * @param category The {@link MetricCategory} this counter is assigned to.
    * @param name A name for this metric.
-   * @param help A String containing helpful information.
+   * @param help A human readable description of the metric.
    * @return The created Counter instance.
    */
   default Counter createCounter(
@@ -42,7 +42,7 @@ public interface MetricsSystem {
    *
    * @param category The {@link MetricCategory} this counter is assigned to.
    * @param name A name for this metric.
-   * @param help A String containing helpful information.
+   * @param help A human readable description of the metric.
    * @param labelNames An array of labels to assign to the Counter.
    * @return The created LabelledMetric instance.
    */
@@ -54,7 +54,7 @@ public interface MetricsSystem {
    *
    * @param category The {@link MetricCategory} this timer is assigned to.
    * @param name A name for this metric.
-   * @param help A String containing helpful information.
+   * @param help A human readable description of the metric.
    * @return The created Timer instance.
    */
   default OperationTimer createTimer(
@@ -67,7 +67,7 @@ public interface MetricsSystem {
    *
    * @param category The {@link MetricCategory} this timer is assigned to.
    * @param name A name for this metric.
-   * @param help A String containing helpful information.
+   * @param help A human readable description of the metric.
    * @param labelNames An array of labels to assign to the Timer.
    * @return The created LabelledMetric instance.
    */
@@ -77,9 +77,11 @@ public interface MetricsSystem {
   /**
    * Creates a gauge for displaying double vales.
    *
+   * A gauge is a metric to report the current value. The metric value may go up or down.
+   *
    * @param category The {@link MetricCategory} this gauge is assigned to.
    * @param name A name for this metric.
-   * @param help A String containing helpful information.
+   * @param help A human readable description of the metric.
    * @param valueSupplier A supplier for the double value to be presented.
    */
   void createGauge(MetricCategory category, String name, String help, DoubleSupplier valueSupplier);
@@ -89,7 +91,7 @@ public interface MetricsSystem {
    *
    * @param category The {@link MetricCategory} this gauge is assigned to.
    * @param name A name for this metric.
-   * @param help A String containing helpful information.
+   * @param help A human readable description of the metric.
    * @param valueSupplier A supplier for the integer value to be presented.
    */
   default void createIntegerGauge(
@@ -105,7 +107,7 @@ public interface MetricsSystem {
    *
    * @param category The {@link MetricCategory} this gauge is assigned to.
    * @param name A name for this metric.
-   * @param help A String containing helpful information.
+   * @param help A human readable description of the metric.
    * @param valueSupplier A supplier for the long value to be presented.
    */
   default void createLongGauge(
